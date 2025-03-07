426d0743612f45515aa3569c093f77420388c3f8
muaadhnoman <muaadhnoman@gmail.com>
2025-03-07 03:46:12 +0300

    Initial Commit

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+# --- PAGE SETUP ---
+about_page = st.Page(
+    page="views/about_me.py",
+    title="About Me",
+    icon=":material/account_circle:",
+    default=True
+)
+
+project_1_page = st.Page(
+    page="views/sales_dashboard.py",
+    title="Sales Dashboard",
+    icon=":material/bar_chart:"
+)
+
+project_2_page = st.Page(
+    page="views/chatbot.py",
+    title="Chat Bot",
+    icon=":material/smart_toy:"
+)
+
+# --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
+pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+# --- SHARED ON ALL PAGES ---
+st.logo("assets/logo.png")
+st.sidebar.markdown("Made with ❤️ by [Muaadh]")
+
+
+# --- RUN NAVIGATION ---
+pg.run()
